25f75e07b68be3b392a7167d68c69aa6a34b9aaa
lujainbakh <lujainbakhurji@gmail.com>
2024-08-13 11:21:13 +0300

    initial commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <span>
+      <img
+        src={"/archiva-logo.png"}
+        alt="Archiva Logo"
+        style={{ height: "25px", marginRight: "10px" }}
+      />
+    </span>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/LujainBakh/Archiva_PMO',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/LujainBakh/Archiva_PMO',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'PMO documentation for © Solutions By 42',
   },
 }
 
